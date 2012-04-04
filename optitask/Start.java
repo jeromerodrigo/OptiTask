b53f27139d8bad9c2552fa307173490992eec48c
jeromerodrigo <jeromerodrigo@gmail.com>
2012-04-04 15:02:07 +0800

    Renamed Main.java to Start.java

---
+++ optitask/Start.java
@@ -0,0 +1,38 @@
+package optitask;
+
+import optitask.store.AppPersistence;
+import optitask.ui.AppFrame;
+
+/**
+ * Start.java <br />
+ * Purpose: Acts as a starting point for the application.
+ * @author Jerome
+ * @version 0.8
+ * @since 0.8
+ */
+
+public final class Start {
+
+    /** Stores a single instance of the application persistence module. */
+    private static AppPersistence model;
+
+    /** Stores a single instance of the main user interface. */
+    private static AppFrame view;
+
+    /**
+     * Default constructor for the Start class.
+     * Not Used.
+     */
+    private Start() {
+    }
+
+    /**
+     * This is the entry point of the program.
+     * @param args not used
+     */
+    public static void main(final String[] args) {
+        model = new AppPersistence("optitask.dat");
+        view = new AppFrame(model);
+        view.setVisible(true);
+    }
+}
